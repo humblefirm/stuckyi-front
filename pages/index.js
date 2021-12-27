cf61f3bacf1afa98aeec2b81cc6d2246c54bd438
humblefirm <38067691+humblefirm@users.noreply.github.com>
2021-12-27 18:24:27 +0900

    feat: card

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-import Head from "next/head"
-import { Tip } from "../components/Tip"
-import { Container } from "../components/Container"
+import Head from "next/head";
+import { Tip } from "../components/Tip";
+import { Container } from "../components/Container";
 
 // A Smart Component from Framer
-import Toggle from "https://framer.com/m/Toggle-B5iT.js@X94mtocAj5XithxLESme"
+import Card from "https://framer.com/m/Card-Hz0R.js@J9UIwB5nPOhf4CSKQLLJ";
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Framer</title>
-                <meta name="description" content="Modules" />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>Framer</title>
+        <meta name="description" content="Modules" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
 
-            <Container>
-                <Toggle />
-                <Tip />
-            </Container>
-        </>
-    )
+      <Container>
+        <Card
+          // Using default values:
+          amount="5000"
+          amount2="24%"
+          amount3="3700원"
+          // image={image}
+          text="스투키 팀 짱짱"
+        />
+      </Container>
+    </>
+  );
 }
